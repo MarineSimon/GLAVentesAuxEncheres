009a7d132e7be25fdb0afd2057368ceffb63ca00
maximeguillaume <maxime.guillaume1@gmail.com>
2012-12-17 13:37:17 +0100

    Data modèle fini mais qui demande une relecture par un développeur tiers

--- PILAgenda/PILAgenda-ejb/src/java/persistence/Event.java
+++ PILAgenda/PILAgenda-ejb/src/java/persistence/Event.java
@@ -12,6 +12,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
@@ -49,7 +50,12 @@ public class Event implements Serializable {
     @ManyToOne
     private UserAgenda user;
     
-
+    @ManyToMany(mappedBy="guestsEvents")
+    private Set<Event> guestsEvents;
+    
+    @ManyToMany(mappedBy="acceptedEvents")
+    private Set<Event> acceptedEvents;
+    
     public Long getId() {
         return id;
     }
