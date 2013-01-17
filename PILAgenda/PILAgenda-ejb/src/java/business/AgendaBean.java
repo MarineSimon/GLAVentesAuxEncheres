c8d81ab3e787bc830ab681419eb091911ea2e7ea
Romain Rigault <romainrigault@hotmail.fr>
2013-01-17 15:29:10 +0100

    Revert "recherches agendas fonctionnel"
    This reverts commit ff0a538704494410053874e630c81c106dd3d213.

--- PILAgenda/PILAgenda-ejb/src/java/business/AgendaBean.java
+++ PILAgenda/PILAgenda-ejb/src/java/business/AgendaBean.java
@@ -56,38 +56,6 @@ public class AgendaBean implements AgendaBeanLocal {
         return (List<Agenda>) query.getResultList();
     }
 
-    public List<Agenda> findInAllAgendas(UserAgenda userConnected, String title, String email, String firstname, String lastname) {
-        TypedQuery<Agenda> query = em.createQuery("SELECT a FROM Agenda a WHERE UPPER (a.name) LIKE UPPER (?1) AND UPPER (a.agendaOwner.email) LIKE UPPER (?2) AND UPPER (a.agendaOwner.firstname) LIKE UPPER (?3) AND UPPER (a.agendaOwner.lastname) LIKE UPPER (?4) AND a.access=1", Agenda.class);
-        if (title != "") {
-            query.setParameter(1, title);
-        } else {
-            query.setParameter(1, "%");
-        }
-        if (email != "") {
-            query.setParameter(2, email);
-        } else {
-            query.setParameter(2, "%");
-        }
-        if (firstname != "") {
-            query.setParameter(3, firstname);
-        } else {
-            query.setParameter(3, "%");
-        }
-        if (lastname != "") {
-            query.setParameter(4, lastname);
-        } else {
-            query.setParameter(4, "%");
-        }
-        return (List<Agenda>) query.getResultList();
-    }
-    public List<Agenda> findAgenda(UserAgenda userConnected, String input) {
-        TypedQuery<Agenda> query = em.createQuery("SELECT a FROM Agenda a WHERE a.agendaOwner = ?1 AND a.name LIKE ?2", Agenda.class);
-        query.setParameter(1, userConnected);
-        query.setParameter(2, "%"+input+"%");
-        return (List<Agenda>) query.getResultList();
-    }
-
-
     @Override
     public Agenda findAgenda(Long id) {
         return em.find(Agenda.class, id);
