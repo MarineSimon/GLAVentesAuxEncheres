935c9e6db0cd5c5c91a1dcd8fb354fe5eb6e4d16
Swann COCHARD <swanncochard@gmail.com>
2013-01-31 13:37:50 +0100

    [31-01-2013 13:23] Création de détails d'un article

---
+++ GLAVenteAuxEncheres-ejb/src/java/business/EnchereBeanLocal.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business;
+
+import java.util.Calendar;
+import library.EnchereBeanInterface;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import persistence.Article;
+import persistence.Enchere;
+import persistence.UserEnchere;
+
+/**
+ *
+ * @author Swann
+ */
+@Stateless
+public class EnchereBeanLocal implements EnchereBeanInterface {
+    @PersistenceContext(unitName="GLAVenteAuxEncheres-PU")
+    private EntityManager em;
+    
+    @Override
+    public void addEnchere(Calendar date, int amount, Article article, UserEnchere user) {
+        Enchere e = new Enchere(date,amount,article,user);
+        em.persist(e);
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+}
---
+++ GLAVenteAuxEncheres-ejb/src/java/library/EnchereBeanInterface.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library;
+
+import java.util.Calendar;
+import javax.ejb.Local;
+import persistence.Article;
+import persistence.UserEnchere;
+
+/**
+ *
+ * @author Swann
+ */
+@Local
+public interface EnchereBeanInterface {
+    public void addEnchere(Calendar date, int amount, Article article, UserEnchere user);
+}
