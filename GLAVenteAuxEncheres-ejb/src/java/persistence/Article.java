502d4dc7cd921691915a06f227453f4e3c365256
maximeguillaume <maxime.guillaume1@gmail.com>
2013-01-31 18:01:46 +0100

    Recherche opérationnelle + Fusion avec le bazard à Mewrine

--- GLAVenteAuxEncheres-ejb/src/java/persistence/Article.java
+++ GLAVenteAuxEncheres-ejb/src/java/persistence/Article.java
@@ -5,6 +5,7 @@
 package persistence;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -70,6 +71,7 @@ public class Article implements Serializable {
         this.initialPrice = price;
         this.endDate = date;
         this.picture = picture;
+        this.promotions = new ArrayList<Promotion>();
     }
     public Long getId() {
         return id;
@@ -142,7 +144,31 @@ public class Article implements Serializable {
         }
         return result;
     }
-
+    
+    public boolean haveOneTypeOfPromotion(){
+        return promotions.size() == 1;
+    }
+    
+    public boolean haveTwoTypeOfPromotion(){
+        return promotions.size() == 2;
+    }
+    
+    public boolean haveOnlyDeliveryFreePromotion(){
+        return haveDeliveryFreePromotion() && haveOneTypeOfPromotion();
+    }
+    
+    public boolean haveOnlyGiftCertificatePromotion(){
+        return haveGiftCertificatePromotion() && haveOneTypeOfPromotion();
+    }
+    
+    public boolean haveAlsoDeliveryFreePromotion(){
+        return haveDeliveryFreePromotion() && haveTwoTypeOfPromotion();
+    }
+    
+    public boolean haveAlsoGiftCertificatePromotion(){
+        return haveGiftCertificatePromotion() && haveTwoTypeOfPromotion();
+    }
+    
     public SubCategory getSubCategory() {
         return subCategory;
     }
