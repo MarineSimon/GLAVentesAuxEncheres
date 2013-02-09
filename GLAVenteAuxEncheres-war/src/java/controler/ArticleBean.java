b9b7cec7d42cb20919639628a63c938777b4f072
Solene <solene@example.com>
2013-02-09 15:51:17 +0100

    Solene - 	image vente en cours

--- GLAVenteAuxEncheres-war/src/java/controler/ArticleBean.java
+++ GLAVenteAuxEncheres-war/src/java/controler/ArticleBean.java
@@ -5,6 +5,11 @@
 package controler;
 
 import business.ArticleBeanLocal;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -14,6 +19,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.naming.InitialContext;
@@ -22,6 +28,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpSession;
 import library.ArticleBeanInterface;
 import org.primefaces.context.RequestContext;
+import org.primefaces.event.FileUploadEvent;
 import persistence.Article;
 import persistence.Category;
 import persistence.SubCategory;
@@ -277,4 +284,43 @@ public class ArticleBean {
         articleLocal.addArticle(a);
         return "backToViewAccount";
     }
+    
+    public void upload(FileUploadEvent event) { 
+        System.out.println("test");
+        FacesMessage msg = new FacesMessage("Success! ", event.getFile().getFileName() + " is uploaded.");  
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+        // Do what you want with the file        
+        try {
+            copyFile(event.getFile().getFileName(), event.getFile().getInputstream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    
+    public void copyFile(String fileName, InputStream in) {
+           try {
+             
+             
+                // write the inputStream to a FileOutputStream
+                OutputStream out = new FileOutputStream(new File(destination + fileName));
+             
+                int read = 0;
+                byte[] bytes = new byte[1024];
+             
+                while ((read = in.read(bytes)) != -1) {
+                    out.write(bytes, 0, read);
+                }
+             
+                in.close();
+                out.flush();
+                out.close();
+             
+                System.out.println("New file created!");
+                } catch (IOException e) {
+                System.out.println(e.getMessage());
+                }
+    }
+    
+    
 }
